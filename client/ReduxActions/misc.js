f1aa90cc1017f73fd055122b1b012a7d60224f4d
Stuart Walsh <stu@ipng.org.uk>
2017-05-10 13:17:41 +0100

    split up the action creators into multiple files to make things a bit more managable

---
+++ client/ReduxActions/misc.js
@@ -0,0 +1,42 @@
+export function navigate(path, search) {
+    return {
+        type: 'NAVIGATE',
+        newPath: path,
+        search: search
+    };
+}
+
+export function setContextMenu(menu) {
+    return {
+        type: 'SET_CONTEXT_MENU',
+        menu: menu
+    };
+}
+
+export function zoomCard(card) {
+    return {
+        type: 'ZOOM_CARD',
+        card: card
+    };
+}
+
+export function clearZoom() {
+    return {
+        type: 'CLEAR_ZOOM'
+    };
+}
+
+export function receiveBannerNotice(notice) {
+    return {
+        type: 'RECEIVE_BANNER_NOTICE',
+        notice: notice
+    };
+}
+
+export function refreshUser(user, token) {
+    return {
+        type: 'REFRESH_USER',
+        user: user,
+        token: token
+    };
+}
